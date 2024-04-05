189fb2b8e5e4bbd745d152dd7e4a26d7405c8fb3
Anton Mikhnou <anton_mihnov03@mail.ru>
2024-04-05 14:14:35 +0300

    can chagnge all but not picture

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const degrees = document.querySelector('.mobile__weather-degrees');
 const town = document.querySelector('.mobile__location-town');
 const country = document.querySelector('.mobile__location-country');
 const form = document.querySelector('form');
+const type = document.querySelector('.mobile__weather-type');
+const picture = document.querySelector('.mobile__picture');
+const img = document.querySelector('img');
 
 // const seachTown = input.value;
 
@@ -10,7 +13,11 @@ async function getWeather(param) {
     const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=fbdeb1a928fa4975a25193301242903&q=${input.value}`, {mode: 'cors'});
     const data = await response.json();
     console.log(data);
+    type.textContent = `${data.current.condition.text}`
     degrees.textContent = `${data.current['temp_c']}°C`
+    town.textContent = `${data.location.name}`
+    country.textContent = `${data.location.country}`
+    picture.src = `${data.current.condition.icon}`
 }
 
 form.addEventListener('submit', (e) => {
