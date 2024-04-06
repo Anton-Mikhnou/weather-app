bdbad4b0d122e845b945c68bbb1b0cadb526921d
Anton Mikhnou <anton_mihnov03@mail.ru>
2024-04-06 10:04:11 +0300

    finish

--- index.js
+++ index.js
@@ -7,14 +7,25 @@ const type = document.querySelector('.mobile__weather-type');
 const picture = document.querySelector('.mobile__picture');
 const error = document.querySelector('.error');
 
+let degreesF;
+let degreesC;
+
 async function getWeather(param) {
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=fbdeb1a928fa4975a25193301242903&q=${param}`, {mode: 'cors'});
-    const data = await response.json();
-    type.textContent = `${data.current.condition.text}`;
-    degrees.textContent = `${data.current['temp_c']}°C`
-    town.textContent = `${data.location.name}`;
-    country.textContent = `${data.location.country}`;
-    picture.src = `${data.current.condition.icon}`;
+    try {
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=fbdeb1a928fa4975a25193301242903&q=${param}`, {mode: 'cors'});
+        const data = await response.json();
+        degreesF = `${data.current['temp_f']}F`;
+        type.textContent = `${data.current.condition.text}`;
+        degreesC = `${data.current['temp_c']}°C`;
+        degrees.textContent = `${data.current['temp_c']}°C`;
+        town.textContent = `${data.location.name}`;
+        country.textContent = `${data.location.country}`;
+        picture.src = `${data.current.condition.icon}`;
+        error.textContent = '';
+        error.className = 'error'
+    } catch (e) {
+        showError(param);
+    } 
 }
 
 getWeather('Minsk');
@@ -26,4 +37,19 @@ form.addEventListener('submit', (e) => {
     const place = obj.town;
     getWeather(place);
 })
+
+function showError(er) {
+    error.className  = 'error active';
+    error.textContent = `We don't know about place like ${er}`;
+}
+
+degrees.addEventListener('click', () => {
+    if (degrees.textContent === degreesC) {
+        degrees.textContent = degreesF;
+    } else {
+        degrees.textContent = degreesC;
+    }
+})
+
+
 
